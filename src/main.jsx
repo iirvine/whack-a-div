71ee7d53404277e695d7444daf6f4e0d406af8ca
iirvine <icirvine@gmail.com>
2015-03-25 20:04:39 -0700

    surface updates width/height

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Surface from './surface';
 
+window.React = React;
+
 React.render(<Surface/>,
   document.getElementById('app'));
--- src/surface.jsx
+++ src/surface.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
 const Surface = React.createClass({
+  getInitialState() {
+    return {width: window.innerWidth, height: window.innerHeight};
+  },
+
+  updateDimensions() {
+    this.setState({width: window.innerWidth, height: window.innerHeight});
+  },
+
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  },
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  },
+
   render() {
-    return <div>Surface</div>;
+    let {width, height} = this.state;
+
+    return <div style={{width: width, height: height}}>Surface</div>;
   }
 });
 
